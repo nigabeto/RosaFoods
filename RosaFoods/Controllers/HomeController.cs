da55b93be737adb0803df706d9e1d5046a89d155
nigabeto <joserpinto@gmail.com>
2024-05-07 10:19:29 -0300

    Criação do projeto RosaFoods

---
+++ RosaFoods/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RosaFoods.Models;
+using System.Diagnostics;
+
+namespace RosaFoods.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
