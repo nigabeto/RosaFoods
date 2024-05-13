86be34ee4d4b6174a0524f57566f8b42108ed0f6
nigabeto <joserpinto@gmail.com>
2024-05-13 12:47:55 -0300

    Definidas as pastas ViewComponent na solução e na pasta Shared para criação do menu link em _LayoutView para exibição do  total de itens no carrinho de compras.

---
+++ RosaFoods/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RosaFoods.Models;
+using RosaFoods.ViewModels;
+
+namespace RosaFoods.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(),
+            //    new CarrinhoCompraItem()
+            //};
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+
+        
+            
+            
+            //public IViewComponentResult Invoke()
+        //{
+        //    //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            
+
+        //    var itens = new List<CarrinhoCompraItem>()
+        //    {
+        //        new CarrinhoCompraItem(),
+        //        new CarrinhoCompraItem()
+        //    };
+
+        //    _carrinhoCompra.CarrinhoCompraItems = itens;
+
+        //    var carrinhoCompraVM = new CarrinhoCompraViewModel
+        //    {
+        //        CarrinhoCompra = _carrinhoCompra,
+        //        CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+        //    };
+
+        //    return View(carrinhoCompraVM);
+        //}
+    }
+}
